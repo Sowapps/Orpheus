c96557db7251f37d90300551918d909f19cecee9
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-01-20 22:31:57 +0000

    First steps

---
+++ trunk/configs/constants.php
@@ -0,0 +1,27 @@
+<?php
+/* includes/constants.php -> CONSTANTS
+ * All web site constants.
+ * Required for initialization.
+ *
+ * Auteur: Florent Hazard.
+ * Revision: 1
+ */
+
+define('TIME', $_SERVER['REQUEST_TIME']);
+define('DS', DIRECTORY_SEPARATOR);
+define('INSIDE', true);
+
+//Useful paths.
+define('CONFPATH',		'config'.DS);
+define('MODPATH',		'modules'.DS);
+define('LIBSPATH',		'libs'.DS);
+define('LOGSPATH',		'logs'.DS);
+define('JSPATH',		'js'.DS);
+define('THEMESPATH',		'themes'.DS);
+
+define('DEFAULTMOD',		'js'.DS);
+
+//Configuration constante du site.
+define('SITENAME',				'My FrameWork');
+
+define('SYSLOGFILENAME',		'.system');
