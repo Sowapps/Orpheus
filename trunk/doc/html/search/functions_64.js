41b6dd6a861dacb7706bfc1b04930a182557ba3f
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-03-01 09:05:11 +0000

    Adding DELETE and INSERT SQL mapping Generating new documentation

--- trunk/doc/html/search/functions_64.js
+++ trunk/doc/html/search/functions_64.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['delete',['delete',['../class_abstract_table.html#a49ac7c15c91dba6ab00b770a60844065',1,'AbstractTable']]],
+  ['delete',['delete',['../class_abstract_table.html#a49ac7c15c91dba6ab00b770a60844065',1,'AbstractTable\delete()'],['../class_s_q_l_mapper___my_s_q_l.html#a61c99821b59d6008f0ea5556164bb8e9',1,'SQLMapper_MySQL\delete()'],['../class_s_q_l_mapper.html#ad12496940845d45eaf877016e7355961',1,'SQLMapper\delete()']]],
   ['deletesecuritycode',['deleteSecurityCode',['../__core_8php.html#a1e0952ded158ae3a3448b6b3ec2d4602',1,'_core.php']]],
   ['display',['display',['../class_rendering.html#a63c95040f0a6cfc837cf79de7f9b36a0',1,'Rendering']]],
   ['doshow',['doShow',['../class_rendering.html#a16a9e7993eeda3a57d8e5ec6e2915eec',1,'Rendering']]]
