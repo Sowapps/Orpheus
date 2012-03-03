7e42c90eac046081e14f162724e10848936378ae
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-03-03 20:32:44 +0000

    [DEBUG] SQL Mapper

--- modules/home.php
+++ modules/home.php
@@ -1,3 +1,7 @@
 <?php 
 ?>
-This page is an example of Home using Orpheus.
+This page is an example of Home using Orpheus.<br />
+<br />
+<?php
+var_dump(SQLMapper::select(array('table'=>'test')));
+?>
