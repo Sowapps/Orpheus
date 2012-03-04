e2ae36eac6cc8880e91b8eedee417b27d59b07d9
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-03-04 21:48:21 +0000

    Improving Permanent object class Adding some documentation for Permanent object class

--- trunk/libs/permanentobject_class.php
+++ trunk/libs/permanentobject_class.php
@@ -1,30 +1,26 @@
 <?php
-/* class/abstracttable_class.php
- * PHP File for class: AbstractTable
- * Classe abstraite de surcharge de table de base de données.
- *
- * Author: Florent Hazard (Cartman34).
- * Revision: 6
- * 
- * Requiert:
- * pdo_query()
- * user_can()
- */ 
-
-abstract class AbstractTable {
+//! The Permanent Object class
+/*!
+	Create permanent object using the SQL Mapper.
+*/
+abstract class PermanentObject {
 	
 	//Attributes
-	
-	protected $modFields = array();
-	protected $data = array();
 	protected static $table = null;
 	protected static $fields = array();
 	protected static $userEditableFields = array();
+	
 	protected static $IDFIELD = 'id';
+	protected $modFields = array();
+	protected $data = array();
 	
 	// *** OVERLOADED METHODS ***
 	
-	public function __construct($data) {
+	//! Constructor
+	/*!
+		\param $data An array of the object's data to construct. 
+	 */
+	public function __construct(array $data) {
 		foreach( static::$fields as $fieldname ) {
 			if( !isset($data[$fieldname]) ) {
 				throw new FieldNotFoundException($fieldname);
@@ -34,9 +30,16 @@ abstract class AbstractTable {
 		$this->modFields = array();
 	}
 	
+	//! Magic getter
+	/*!
+		\param $name Name of the property to get.
+		\return The value of field $name.
+		
+		Get the value of field $name.
+	*/
 	public function __get($name) {
 		try {
-			return $this->getData($name);
+			return $this->getValue($name);
 		} catch(FieldNotFoundException $e) {
 			/* Previously, we got the attribute if
 			 * the data does not exist but private is private.
@@ -45,9 +48,16 @@ abstract class AbstractTable {
 		}
 	}
 	
+	//! Magic setter
+	/*!
+		\param $name Name of the property to set.
+		\param $value New value of the property.
+		
+		Set the value of field $name.
+	*/
 	public function __set($name, $value) {
 		try {
-			$this->setData($name, $value);
+			$this->setValue($name, $value);
 		} catch(FieldNotFoundException $e) {
 			/* Previously, we set the attribute if
 			 * the data does not exist but private is private.
@@ -56,6 +66,10 @@ abstract class AbstractTable {
 		}
 	}
 	
+	//! Destructor
+	/*!
+		If something was modified, it saves the new data.
+	*/
 	public function __destruct() {
 		if( !empty($this->modFields) ) {
 			try {
@@ -67,16 +81,31 @@ abstract class AbstractTable {
 		}
 	}
 	
+	//! Magic toString
+	/*!
+		\return The string value of the object.
+		
+		The object's value when casting to string.
+	*/
 	public function __toString() {
 		return '#'.$this->{static::$IDFIELD}.' ('.static::getClass().')';
 	}
 	
 	// *** USER METHODS ***
 	
-	public static function getClass() {
-		return __CLASS__;
-	}
-	
+	//! Update object
+	/*!
+		\param $uInputData The input data we will check and extract, used by children.
+		\param $uInputData The data from wich it will update this object, used by parents, including this one.
+		\return 1 in case of success, else 0.
+		\sa runForUpdate()
+		
+		This method require to be overrided but it still be called too by the child classes.
+		Here $uInputData is not used, it is reserved for child classes.
+		$data must contain a filled array of new data.
+		This method update the EDIT event log.
+		Before saving, runForUpdate() is called to let child classes to run custom instructions.
+	*/
 	public function update($uInputData, array $data=array()) {
 		try {
 			if( empty($data) ) {
@@ -86,7 +115,7 @@ abstract class AbstractTable {
 		} catch(UserException $e) { addUserError($e); return 0; }
 		
 		foreach($data as $fieldname => $fieldvalue) {
-			if( $fieldname != static::$IDFIELD && (user_can(static::$table.'_edit') || in_array($fieldname, static::$userEditableFields)) ) {
+			if( $fieldname != static::$IDFIELD && in_array($fieldname, static::$userEditableFields) ) {
 				$this->$fieldname = $fieldvalue;
 			}
 		}
@@ -97,8 +126,21 @@ abstract class AbstractTable {
 		return $this->save();
 	}
 	
+	//! Run for Update
+	/*!
+		\sa update()
+		
+		This function is called by update() before saving new data.
+		In this base class, this method does nothing.
+	*/
 	public function runForUpdate() { }
 	
+	//! Save object
+	/*!
+		\return 1 in case of success, else 0.
+		
+		If some fields was modified, it saves these fields using the SQL Mapper.
+	*/
 	public function save() {
 		if( empty($this->modFields) ) {
 			return 0;
@@ -120,13 +162,42 @@ abstract class AbstractTable {
 		return SQLMapper::doUpdate($options);
 	}
 	
+	//! Mark the field as modified
+	/*!
+		\param $field The field to mark as modified.
+		
+		Add the $field to the modified fields array.
+	*/
 	private function addModFields($field) {
 		if( !in_array($field, $this->modFields) ) {
 			$this->modFields[] = $field;
 		}
 	}
 	
-	public function setData($key, $value) {
+	//! Get one value or all values.
+	/*!
+		\param $key Name of the field to get.
+		
+		Get the value of field $key or all data values if $key is null.
+	*/
+	public function getValue($key=null) {
+		if( !empty($key) ) {
+			if( !in_array($key, static::$fields) ) {
+				throw new FieldNotFoundException($key);
+			}
+			return $this->data[$key];
+		}
+		return $this->data;
+	}
+	
+	//! Set the value of a field
+	/*!
+		\param $key Name of the field to set.
+		\param $value New value of the field.
+		
+		Set the field $key with the new $value.
+	*/
+	public function setValue($key, $value) {
 		if( !isset($key, $value) ) {
 			throw new UserException("nullValue");
 			
@@ -141,24 +212,30 @@ abstract class AbstractTable {
 		}
 	}
 	
-	public function getData($key=null) {
-		if( !empty($key) ) {
-			if( !in_array($key, static::$fields) ) {
-				throw new FieldNotFoundException($key);
-			}
-			return $this->data[$key];
-		}
-		return $this->data;
-	}
-	
-	public function equals(AbstractTable $o) {
+	//! Verify equality
+	/*!
+		\param $o The object to compare.
+		\return True if this object represents the same data, else False.
+		
+		Compare the class and the ID field value of the 2 objects.
+	*/
+	public function equals(PermanentObject $o) {
 		return (static::getClass()==$o::getClass() && $this->{static::$IDFIELD}==$o->{static::$IDFIELD});
 	}
 	
-	public function logEvent($event, $time=null, $addIP=null) {
-		$log = static::getLogEvent($event, $time, $addIP);
-		$this->setData($event.'_time', $log[$event.'_time']);
-		$this->setData($event.'_ip', $log[$event.'_ip']);
+	//! Log an event
+	/*!
+		\param $event The event to log in this object.
+		\param $time A specified time to use for logging event.
+		\param $ipAdd A specified IP Adress to use for logging event.
+		\sa getLogEvent()
+		
+		Compare the class and the ID field value of the 2 objects.
+	*/
+	public function logEvent($event, $time=null, $ipAdd=null) {
+		$log = static::getLogEvent($event, $time, $ipAdd);
+		$this->setValue($event.'_time', $log[$event.'_time']);
+		$this->setValue($event.'_ip', $log[$event.'_ip']);
 	}
 	
 	// *** STATIC METHODS ***
@@ -224,6 +301,15 @@ abstract class AbstractTable {
 		return $LastInsert;
 	}
 	
+	public static function completeFields($data) {
+		foreach( static::$fields as $fieldname ) {
+			if( !isset($data[$fieldname]) ) {
+				$data[$fieldname] = '';
+			}
+		}
+		return $data;
+	}
+	
 	public static function getLogEvent($event, $time=null, $addIP=null) {
 		if( !isset($time) ) {
 			$time=time();
@@ -242,13 +328,8 @@ abstract class AbstractTable {
 		return static::$IDFIELD;
 	}
 	
-	public static function completeFields($data) {
-		foreach( static::$fields as $fieldname ) {
-			if( !isset($data[$fieldname]) ) {
-				$data[$fieldname] = '';
-			}
-		}
-		return $data;
+	public static function getClass() {
+		return __CLASS__;
 	}
 	
 	public static function runForEntry(&$data) { }
