85be7e5f43452e2d56a6cbd8319d0fa2ac9c4ed8
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-03-06 19:56:52 +0000

    Documenting Hook class. Generating new documentation. [FIX] Possible issue with trigger parameters.

--- trunk/doc/html/search/all_67.js
+++ trunk/doc/html/search/all_67.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['gensecuritycode',['genSecurityCode',['../__core_8php.html#a5e826534fd3a3d07cc52032f416f8302',1,'_core.php']]],
   ['get',['get',['../class_config_core.html#a4bcb7f4e5aa87651dcba4ced7962a61f',1,'ConfigCore\get()'],['../class_permanent_object.html#a4c6aa9d927b84a9e5ad68a81c0205ed9',1,'PermanentObject\get()']]],
-  ['getclass',['getClass',['../class_permanent_object.html#a6ba5fefedb61a22bbf3fc3eb513e2300',1,'PermanentObject']]],
   ['getcsspath',['getCSSPath',['../class_h_t_m_l_rendering.html#a59ab6f1a8ab47432b14811a3eba088e1',1,'HTMLRendering']]],
   ['getfieldname',['getFieldName',['../class_field_not_found_exception.html#a3c02f8c6a00687928c4aa46650d3cc9e',1,'FieldNotFoundException']]],
   ['getidfield',['getIDField',['../class_permanent_object.html#a2a2d396a9fc3f4cdd42fa4704bb83f36',1,'PermanentObject']]],
