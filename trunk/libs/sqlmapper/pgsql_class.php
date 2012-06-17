a4e26566bddca7d78b59a1379c5b8f3fe0150c53
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-06-17 19:04:01 +0000

    [FIX] invalid pgsql insert query.

--- trunk/libs/sqlmapper/pgsql_class.php
+++ trunk/libs/sqlmapper/pgsql_class.php
@@ -153,7 +153,7 @@ class SQLMapper_PgSQL extends SQLMapper {
 			text((empty($options['what'][0])) ? "No first row" : "First row ok." );
 			if( empty($options['what'][0]) ) {
 				text("We got an assoc array");
-				$options['what'][0] = $options['what'];
+				$options['what'] = array($options['what']);
 			}// Else it's an indexed array of fields Arrays
 			$COLS = '('.implode(', ', array_keys($options['what'][0])).')';
 			foreach($options['what'] as $row) {
