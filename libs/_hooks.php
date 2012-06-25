897abb7a5815b8701c02a1c3b5817f8b0ad6b53d
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-06-25 08:50:06 +0000

    Filling $USER variable.

--- libs/_hooks.php
+++ libs/_hooks.php
@@ -22,6 +22,13 @@ Hook::register('runModule', function ($Module) {
 	}
 });
 
+Hook::register('checkModule', function () {
+	if( User::is_login() ) {
+		global $USER;
+		$USER = &$_SESSION['USER'];
+	}
+});
+
 // if( !empty($_GET['module']) && is_name($_GET['module']) && file_exists(MODPATH.$_GET['module'].'.php') ) {
 // 	if( user_access($_GET['module']) ) {
 // 		$Module = $_GET['module'];
