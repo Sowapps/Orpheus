07b45b8f1ef8e522d29b938aff9b7e1b9e83b558
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-06-25 09:02:56 +0000

    [DEBUG] Invalid variable $USER.

--- trunk/modules/user_login.php
+++ trunk/modules/user_login.php
@@ -23,7 +23,7 @@ if( !empty($_POST['submitLogin']) ) {
 }
 if( User::is_login() ) {
 	var_dump($USER);
-	echo "Welcome {$USER->fullname} !<br />";
+	//echo "Welcome {$USER->fullname} !<br />";
 }
 ?>
 <h2>Login</h2>
