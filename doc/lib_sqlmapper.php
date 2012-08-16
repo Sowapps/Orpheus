2579e11d68d5c1f939b8481ac59ba8e9063855c0
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-08-16 17:45:25 +0000

    Documenting basic libraries (finished). Minor changes and some documentation. Generating new documentation.

---
+++ doc/lib_sqlmapper.php
@@ -0,0 +1,18 @@
+<?php
+/*!
+ * \page lib_sqlmapper SQLMapper Library
+ * 
+ * \section intro_sec Introduction
+ *
+ * The SQLMapper library is designed to map SQL entities into PHP OBbjects.\n
+ * It includes a small PDO library to use easily PDO as procedural programming.\n
+ * Each driver requires is own Mapper, inherited from the SQlMapper class.\n
+ * Only the mappers for MySQL and PostgreSQL DBMS were developed for the moment.\n
+ * \n
+ * This package is often use by libraries, we recommend it.\n
+ * 
+ * \section install_sec Installation
+ * 
+ * Copy all files from the archive into your website root directory.\n
+ * Edit the configs/database.ini file with your database settings.\n
+ */
