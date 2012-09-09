35b6bc1ac7c41370d90221ec7be38661732ae49b
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-09-09 07:55:52 +0000

    Updating documentation. Generating new documentation.

--- trunk/doc/html/search/variables_65.js
+++ trunk/doc/html/search/variables_65.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['else',['else',['../index_8php.html#ab5df36c051d50c88abddee870c022f42',1,'else():&#160;index.php'],['../settings_8php.html#a25fd151a97db1f0c99d551050c85c85c',1,'else():&#160;settings.php']]],
   ['error_5flevel',['ERROR_LEVEL',['../constants_8php.html#ae7d76501599cdeb7245580b881cf2d17',1,'constants.php']]]
 ];
