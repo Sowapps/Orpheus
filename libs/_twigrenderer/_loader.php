7dd5ae18f124b49a8692d96990ccfb7f30c58572
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-09-17 20:42:20 +0000

    [FIX] Twig rendering class not found.

--- libs/_twigrenderer/_loader.php
+++ libs/_twigrenderer/_loader.php
@@ -5,13 +5,14 @@
  */
 
 if( function_exists('log_debug') ) {
-	log_debug("TwigRendering loader: Checkin if we shoudl load it.");
+	log_debug("TwigRendering loader: Checkin if we should load it.");
 }
 if( strtolower(Config::get('default_rendering')) != 'twigrendering' ) {
 	return;// We don't want to load a not used library.
 }
 
 addAutoload('TwigRendering', '_twigrenderer/twigrendering_class.php');
+text($AUTOLOADS);
 text('TwigRendering loader: TwigRendering loaded.');
 if( function_exists('log_debug') ) {
 	log_debug("TwigRendering loader: TwigRendering loaded.");
