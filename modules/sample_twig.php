b77fbaad7ed43c230b5556f3bc45eb5a07306276
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-09-17 20:21:31 +0000

    [DEBUG] Problem with twig, only displays a blank page.

--- modules/sample_twig.php
+++ modules/sample_twig.php
@@ -1,5 +1,4 @@
 <?php
 //TwigRendering::display($Module);
 Config::set('default_rendering', 'TwigRendering');
-log_debug('default_rendering config setted to TwigRendering');
-log_debug('checking...'.Config::get('default_rendering'));
+includeDir(LIBSPATH.'_twigrendering');
