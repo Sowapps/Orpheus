a8e0d77f83c477c07edc2efed5c459ed26def2a6
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-09-17 20:47:18 +0000

    [DEBUG] Twig rendering class not found.

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ function __autoload($className) {
 		// PHP's class name are not case sensitive.
 		$bFile = strtolower($className);
 		
-		if( text('log_debug') ) { text(print_r($AUTOLOADS, 1)."\nSearching $bFile"); }
+		if( function_exists('text') ) { text(print_r($AUTOLOADS, 1)."\nSearching $bFile"); }
 		if( function_exists('log_debug') ) { log_debug(print_r($AUTOLOADS, 1)."\nSearching $bFile"); }
 		
 		// If the class file path is known in the AUTOLOADS array
