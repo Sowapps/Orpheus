256b102c79cbab4d8480e139f797665544c9f531
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-09-17 21:00:22 +0000

    [DEBUG] Undefined variables.

--- trunk/libs/_twigrenderer/_loader.php
+++ trunk/libs/_twigrenderer/_loader.php
@@ -7,16 +7,17 @@
 if( function_exists('log_debug') ) {
 	log_debug("TwigRendering loader: Checking if we should load it.");
 }
-*/
 text("TwigRendering loader: Checking if we should load it.");
 if( strtolower(Config::get('default_rendering')) != 'twigrendering' ) {
 	return;// We don't want to load a not used library.
 }
+*/
 
 addAutoload('TwigRendering', '_twigrenderer/twigrendering_class.php');
+/*
 text($AUTOLOADS);
 text('TwigRendering loader: TwigRendering loaded.');
-/*
+
 if( function_exists('log_debug') ) {
 	log_debug("TwigRendering loader: TwigRendering loaded.");
 }
@@ -26,5 +27,6 @@ require_once dirname(__FILE__).'/Twig/lib/Twig/Autoloader.php';
 
 Twig_Autoloader::register();
 
-text("Init TwigRendering");
+//text("Init TwigRendering");
+
 TwigRendering::init();
