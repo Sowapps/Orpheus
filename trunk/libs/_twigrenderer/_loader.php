a7fc063bb1039213001dd9f49a5f965b735ec761
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-09-17 20:29:50 +0000

    [FIX] Including twig renderer loader file.

--- trunk/libs/_twigrenderer/_loader.php
+++ trunk/libs/_twigrenderer/_loader.php
@@ -11,7 +11,7 @@ if( strtolower(Config::get('default_rendering')) != 'twigrendering' ) {
 	return;// We don't want to load a not used library.
 }
 
-addAutoload('TwigRendering', '_twigrendering/twigrendering_class.php');
+addAutoload('TwigRendering', '_twigrenderer/twigrendering_class.php');
 if( function_exists('log_debug') ) {
 	log_debug("TwigRendering loader: TwigRendering loaded.");
 }
