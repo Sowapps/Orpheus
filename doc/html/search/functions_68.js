95151dc20211eb50eb9f2ca66f04ee9f3ed02596
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2012-09-23 16:27:19 +0000

    Minor changes about menus. Generating new documentation.

--- doc/html/search/functions_68.js
+++ doc/html/search/functions_68.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['hashpassword',['hashPassword',['../class_user.html#a6d010f0d182dd4f7a65d16b651237e3e',1,'User']]],
   ['htmlredirectto',['htmlRedirectTo',['../__core_8php.html#aed2c1eb83649c7eeb4f940c8b06a42b2',1,'_core.php']]],
-  ['htmlselect',['htmlSelect',['../__core_8php.html#a3209cfe7e958186055f3a8d639b65288',1,'_core.php']]],
+  ['htmlselect',['htmlSelect',['../__core_8php.html#aafc8ae2541f68ee1529f16a5829f980b',1,'_core.php']]],
   ['htmlvalue',['htmlValue',['../__core_8php.html#a868d635f811c04d1d71ced4e8d05b65b',1,'_core.php']]]
 ];
