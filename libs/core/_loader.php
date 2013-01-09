a6faef06778022f5341c96381a8fa1682d17561f
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-01-09 15:12:23 +0000

    Re-ordering core files inclusion.

--- libs/core/_loader.php
+++ libs/core/_loader.php
@@ -2,8 +2,15 @@
 /* Loader File for the core sources
  */
 
+echo __FILE__.' : '.__LINE__."<br />\n";
 require_once LIBSPATH.'core/core.php';
-require_once LIBSPATH.'core/hooks.php';
-require_once LIBSPATH.'core/validators.php';
+
+echo __FILE__.' : '.__LINE__."<br />\n";
 
 addAutoload('ConfigCore',						'core/configcore_class.php');
+
+echo __FILE__.' : '.__LINE__."<br />\n";
+require_once LIBSPATH.'core/hooks.php';
+echo __FILE__.' : '.__LINE__."<br />\n";
+require_once LIBSPATH.'core/validators.php';
+echo __FILE__.' : '.__LINE__."<br />\n";
