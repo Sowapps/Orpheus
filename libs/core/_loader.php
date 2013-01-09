96f4cb81fcd31506606a9aa74d19cac36bc81197
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-01-09 15:04:45 +0000

    Re-ordering core files inclusion.

--- libs/core/_loader.php
+++ libs/core/_loader.php
@@ -2,4 +2,8 @@
 /* Loader File for the core sources
  */
 
+require_once LIBSPATH.'core/core.php';
+require_once LIBSPATH.'core/hooks.php';
+require_once LIBSPATH.'core/validators.php';
+
 addAutoload('ConfigCore',						'core/configcore_class.php');
