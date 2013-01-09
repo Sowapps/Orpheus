daad4b1976337639f7b0df595a0e7f214ea577dd
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-01-09 16:15:45 +0000

    Cleaning

--- libs/core/_loader.php
+++ libs/core/_loader.php
@@ -2,15 +2,9 @@
 /* Loader File for the core sources
  */
 
-echo __FILE__.' : '.__LINE__."<br />\n";
 require_once LIBSPATH.'core/core.php';
-
-echo __FILE__.' : '.__LINE__."<br />\n";
 
 addAutoload('ConfigCore',						'core/configcore_class.php');
 
-echo __FILE__.' : '.__LINE__."<br />\n";
 require_once LIBSPATH.'core/hooks.php';
-echo __FILE__.' : '.__LINE__."<br />\n";
 require_once LIBSPATH.'core/validators.php';
-echo __FILE__.' : '.__LINE__."<br />\n";
