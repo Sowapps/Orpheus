acf02285a0e4324df154f37fb6403520a7915581
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-01-16 20:25:48 +0000

    Better error management when it occured during the core initializing process. Better display too.

--- trunk/libs/core/_loader.php
+++ trunk/libs/core/_loader.php
@@ -2,9 +2,13 @@
 /* Loader File for the core sources
  */
 
+// Core
 require_once LIBSPATH.'core/core.php';
 
+// Important
 addAutoload('ConfigCore',						'core/configcore_class.php');
 
 require_once LIBSPATH.'core/hooks.php';
 require_once LIBSPATH.'core/validators.php';
+
+addAutoload('UserException',					'core/userexception_class.php');
