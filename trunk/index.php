58a1f91e13c5fddc96d8e28478eb99fc4f1f13e4
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-04-25 08:09:40 +0000

    Updating Orpheus to new Permanent Object and better global management.

--- trunk/index.php
+++ trunk/index.php
@@ -1,67 +1,14 @@
 <?php
 /*!
-	\file index.php
-	\brief The WebSite Core
-	\author Florent Hazard
-	\copyright The MIT License, see LICENSE.txt
-	
-	PHP File for the website core.
+ * \file index.php
+ * \brief The Orpheus Core
+ * \author Florent Hazard
+ * \copyright The MIT License, see LICENSE.txt
+ * 
+ * PHP File for the website core.
  */
 
-//! Defines an undefined constant.
-/*!
- * \param $name		The name of the constant.
-* \param $value	The value of the constant.
-* \return True if the constant was defined successfully, else False.
-
-* Defines a constant if this one is not defined yet.
-*/
-function defifn($name, $value) {
-	if( defined($name) ) {
-		return false;
-	}
-	define($name, $value);
-	return true;
-}
-
-//! Gets the directory path
-/*!
-	\param $path The path get parent directory
-	\return The secured path
-	
-	Gets the parent directory path of $path
-*/
-function dirpath($path) {
-	$dirname = dirname($path);
-	return ( $dirname == '/' ) ? '/' : $dirname.'/';
-}
-
-//! Gets the path of a file/directory using a preferred directory. 
-/*!
- * \param $prefDir The preferred directory path to use.
- * \param $altDir The alternative directory path.
- * \param $commonPath The common path.
- * 
- * This function tries to return the path to the file $commonPath searching first
- * in the preferred directory $prefDir and if not found in this one, it searches
- * into the alternative directory $altDir.
- * This function does not check if $commonPath file really exists in $altDir directory.
-*/
-function getPath($prefDir, $altDir, $commonPath) {
-	return ( file_exists($prefDir.$commonPath) ) ? $prefDir.$commonPath : $altDir.$commonPath;
-}
-
-//! Gets the path of a file/directory. 
-/*!
- * \param $commonPath The common path.
- * \see getPath()
- * 
- * This function uses getPath() with INSTANCEPATH and ORPHEUSPATH as parameters.
- * It allows developers to get a dynamic path to a file.
-*/
-function pathOf($commonPath) {
-	return getPath(INSTANCEPATH, ORPHEUSPATH, $commonPath);
-}
+require_once 'loader.php';
 
 // This method take care about paths through symbolic links.
 defifn('ORPHEUSPATH', dirpath($_SERVER['SCRIPT_FILENAME']));
