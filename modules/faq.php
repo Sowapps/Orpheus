446367f36c865c892878e255b873ee2f9718da17
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-05-03 06:58:45 +0000

    Updating menus and faq.

--- modules/faq.php
+++ modules/faq.php
@@ -1,25 +1,33 @@
 
-<h1>Frequently Asked Questions</h1><br />
-<h2>Why this website is ugly ?</h2><br />
+<h1>Frequently Asked Questions</h1>
+
+<h2>Why is this website using the most common bootstrap theme ?</h2>
 <p>
 The reason is very simple, the orpheus' developer is... a developer and not a web designer.<br />
-If you're a webdesigner and you would like to propose a style, please contact me at contact[at]cartman34.fr .
+As the framework is standardized, per default, it uses bootstrap framework with the simplest theme.<br />
+If you're a webdesigner and you would like to propose a theme, please contact me at contact[at]cartman34.fr .
 </p>
+
 <h2>Why this website is (only) in english ?</h2>
 <p>
-Because English is an international language, this is the language used by developers to make it understandable by everyone, as the Orpheus framework.
+Because English is an international language, this is the language used by developers to make it understandable by everyone, as the Orpheus framework.<br />
+Even if the framework has an internationalization, its default language is English as the demo website and documentation website.
 </p>
+
 <h2>Is this website based on Orpheus ?</h2>
 <p>
 Yes, of course. This version is often up to date.
 </p>
+
 <h2>How it works ? How can i get started ?</h2>
 <p>
 Dude, please read <a href="doc/html/">the manual of Orpheus</a>. ;-)
 </p>
+
 <h2>Is it just another PHP framework ?</h2>
 <p>
 In fact, this is not really a framework, it's the future base of your website.<br />
 It helps you to complete your work, without coding again the common base of a website.<br />
 The main difference is that Orpheus forces you to follow certain basic rules, allowing you to maintain a secure and optimized application, even if it's designed to be tolerant and extensible.<br />
+ With a lot of library, everything is allowed on Orpheus, you can use your own user system, your own config library or import a library from another framework. 
 </p>
