d59cd056d71e63fa4d61c29903d26158e10bd265
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-05-16 14:27:29 +0000

    Adding MSSQL module to test MS SQL features (with menu item and label).

---
+++ trunk/modules/mssql.php
@@ -0,0 +1,32 @@
+<?php
+if( isPOST('data') ) {
+	try {
+		
+		$tid = DemoTest_MSSQL::create($_POST['data']);
+		reportSuccess("Object created.");
+		
+		$test = DemoTest_MSSQL::load($tid);
+		reportSuccess("Object \"{$test}\" loaded, it's named \"{$test->name}\".");
+		
+		DemoTest_MSSQL::delete($tid);
+		reportSuccess("Object deleted.");
+		
+	} catch (UserException $e) {
+		reportError($e);
+	}
+	
+}
+?>
+<section id="demotest">
+<?php
+displayReportsHTML();
+?>
+
+	<fieldset>
+		<legend>Using multiple DB instance feature and MS SQL server, try to create you own DemoTest object</legend>
+		<label>Create it from a new value</label>
+		<input type="text" name="data[name]" placeholder="Type new value...">
+		<span class="help-block">Submit a new value to see this working test in action.</span>
+		<button id="submitDemoTest" type="submit" class="btn">Insert it !</button>
+	</fieldset>
+</section>
