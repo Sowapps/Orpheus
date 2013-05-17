8cc37abd9594cb143f8533a268178995a9efdc1b
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-05-17 09:30:13 +0000

    [DEBUG] Issue while selecting from MS SQL server.

--- libs/sqladapter/mssql_class.php
+++ libs/sqladapter/mssql_class.php
@@ -143,7 +143,7 @@ class SQLAdapter_MSSQL extends SQLAdapter {
 		$OPTIONS = '';
 		$SUBOPTIONS = '';
 		$SUBOPTIONS .= ( $options['number'] > 0 ) ?
-			' TOP ('.$options['number'].')'.( ($options['number_percent']) ? ' PERCENT' : '' ) : '';
+			' TOP '.$options['number'].( ($options['number_percent']) ? ' PERCENT' : '' ) : '';
 		$WC = ( !empty($options['where']) ) ? 'WHERE '.$options['where'] : '';
 		$ORDERBY = ( !empty($options['orderby']) ) ? 'ORDER BY '.$options['orderby'] : '';
 		
