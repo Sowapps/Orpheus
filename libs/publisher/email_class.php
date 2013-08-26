72e87a4fad4d44a85d4e15c223450214abffe62f
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-08-26 15:10:06 +0000

    Minor changes.

--- libs/publisher/email_class.php
+++ libs/publisher/email_class.php
@@ -1,10 +1,10 @@
 <?php
 //! The email class
 /*!
-	This class is a tool to send mails.
-		
-	Incompatible with Rev < 552
-*/
+ * This class is a tool to send mails.
+ * 	
+ * Incompatible with Rev < 552
+ */
 class Email {
 	//Attributes
 	private $Headers = array(
@@ -43,9 +43,9 @@ class Email {
 	
 	//! Constructor
 	/*!
-		\param $TEXTBody The body of the text message. Default value is an empty string.
-		\param $Subject The subject of the mail. Default value is an empty string.
-	*/
+	 * \param $TEXTBody The body of the text message. Default value is an empty string.
+	 * \param $Subject The subject of the mail. Default value is an empty string.
+	 */
 	public function __construct($Subject='', $Text='') { //Class' Constructor
 		$this->init();
 		$this->setSubject($Subject);
@@ -71,9 +71,9 @@ class Email {
 	
 	//! Sets the value of a header
 	/*!
-		\param $Key The key of the header to set.
-		\param $Value The new value of the header.
-	*/
+	 * \param $Key The key of the header to set.
+	 * \param $Value The new value of the header.
+	 */
 	public function setHeader($Key, $Value) {
 		if( !isset($this->Headers[$Key]) ) {
 			throw new Exception('UnknownHeader');
@@ -84,11 +84,11 @@ class Email {
 	
 	//! Sets the type of the mail
 	/*!
-		\param $Type The new Type.
-		
-		Sets the type of the mail.
-		It can be TEXTTYPE or HTMLTYPE. 
-	*/
+	 * \param $Type The new Type.
+	 * 
+	 * Sets the type of the mail.
+	 * It can be TEXTTYPE or HTMLTYPE. 
+	 */
 	public function setType($Type) {
 		$Type = (int) $Type;
 		if( $Type < 0 ) {
@@ -104,29 +104,29 @@ class Email {
 	
 	//! Checks if this file is in the files list
 	/*!
-		\param $Filename The file name.
-		\return True if this file is in the attached files list.
-	*/
+	 * \param $Filename The file name.
+	 * \return True if this file is in the attached files list.
+	 */
 	public function containsFile($Filename) {
 		return in_array($Filename, $this->AttFiles);
 	}
 	
 	//! Checks if the file list contains any file.
 	/*!
-		\return True if the file list is not empty.
-		
-		Checks if the file list is not empty.
-	*/
+	 * \return True if the file list is not empty.
+	 * 
+	 * Checks if the file list is not empty.
+	 */
 	public function containsFiles() {
 		return !empty($this->AttFiles);
 	}
 	
 	//! Adds a file to the files list
 	/*!
-		\param $Filename The file name.
-		
-		Adds $Filename to the attached files list.
-	*/
+	 * \param $Filename The file name.
+	 * 
+	 * Adds $Filename to the attached files list.
+	 */
 	public function addFile($Filename) {
 		if( $this->containsFile($Filename) ) {
 			throw new Exception('FileAlreadyContained');
@@ -136,10 +136,10 @@ class Email {
 	
 	//! Removes a file from the files list
 	/*!
-		\param $Filename The file name.
-		
-		Removes $Filename from the attached files list.
-	*/
+	 * \param $Filename The file name.
+	 * 
+	 * Removes $Filename from the attached files list.
+	 */
 	public function removeFile($Filename) {
 		if( ($key = array_search($Filename, $this->AttFiles)) === false ) {
 			throw new Exception('FileNotContained');
@@ -149,8 +149,8 @@ class Email {
 	
 	//! Sets the subject of the mail
 	/*!
-		\param $Subject The new subject.
-	*/
+	 * \param $Subject The new subject.
+	 */
 	public function setSubject($Subject) {
 // 		if( !is_string($Subject) ) {
 // 			throw new Exception('RequireStringParameter');
@@ -163,8 +163,8 @@ class Email {
 	
 	//! Sets the text body of the mail
 	/*!
-		\param $Body The new body.
-	*/
+	 * \param $Body The new body.
+	 */
 	public function setTEXTBody($Body) {
 		if( !is_string($Body) ) {
 			throw new Exception('RequireStringParameter');
@@ -174,8 +174,8 @@ class Email {
 
 	//! Sets the html body of the mail
 	/*!
-	 \param $Body The new body.
-	*/
+	 * \param $Body The new body.
+	 */
 	public function setHTMLBody($Body) {
 	if( !is_string($Body) ) {
 	throw new Exception('RequireStringParameter');
@@ -185,10 +185,10 @@ class Email {
 	
 	//! Sets the mail content
 	/*!
-		\param $Text The new text for the mail contents.
-		
-		Fills Text and HTML bodies from the given text
-	*/
+	 * \param $Text The new text for the mail contents.
+	 * 
+	 * Fills Text and HTML bodies from the given text
+	 */
 	public function setText($Text) {
 		if( !is_string($Text) ) {
 			throw new Exception('RequireStringParameter');
@@ -199,8 +199,8 @@ class Email {
 	
 	//! Sets the alternative body of the mail
 	/*!
-		\param $Body The new body.
-	*/
+	 * \param $Body The new body.
+	 */
 	public function setAltBody($Body) {
 		if( !is_string($Subject) ) {
 			throw new Exception('RequireStringParameter');
@@ -210,11 +210,11 @@ class Email {
 	
 	//! Sends the mail to the given address
 	/*!
-		\param $ToAddress The email adress to send this mail
-		
-		Sends the mail to the given adress.
-		You can pass an array of address to send it to multiple recipients.
-	*/
+	 * \param $ToAddress The email adress to send this mail
+	 * 
+	 * Sends the mail to the given adress.
+	 * You can pass an array of address to send it to multiple recipients.
+	 */
 	public function send($ToAddress) {
 		if( empty($ToAddress) || (!self::is_email($ToAddress) && !is_array($ToAddress)) ) {
 			throw new Exception('InvalidEmailAddress');
@@ -398,8 +398,8 @@ BODY;
 	
 	//! Sets the ReplyTo value of the mail
 	/*!
-		\param $Email The email adress to send this mail
-	*/
+	 * \param $Email The email adress to send this mail
+	 */
 	public function setReplyTo($Email) {
 		$this->setHeader('Return-Path', $Email);
 		$this->setHeader('Reply-To', $Email);
@@ -407,11 +407,11 @@ BODY;
 	
 	//! Sets the Sender value of the mail
 	/*!
-		\param $Email The email adress to send this mail
-		
-		Sets the Sender value of the mail.
-		This function also sets the ReplyTo value if undefined.
-	*/
+	 * \param $Email The email adress to send this mail
+	 * 
+	 * Sets the Sender value of the mail.
+	 * This function also sets the ReplyTo value if undefined.
+	 */
 	public function setSender($Email) {
 		$this->setHeader('From', $Email);
 		$this->setHeader('Sender', $Email);
@@ -422,9 +422,9 @@ BODY;
 	
 	//! Gets a boundary
 	/*!
-		\param $BoundaryInd The index of the boundary to get. Default value is 0.
-		\return The value of the boundary.
-	*/
+	 * \param $BoundaryInd The index of the boundary to get. Default value is 0.
+	 * \return The value of the boundary.
+	 */
 	public function getBoundary($BoundaryInd=0) {
 		if( empty($this->MIMEBoundary[$BoundaryInd]) ) {
 			$this->MIMEBoundary[$BoundaryInd] = '-=%ORPHEUS_'.md5(microtime(1)+$BoundaryInd).'%=-';
@@ -434,50 +434,50 @@ BODY;
 	
 	//! Checks if this mail is a HTML mail
 	/*!
-		\return True if this object has a HTML message.
-	*/
+	 * \return True if this object has a HTML message.
+	 */
 	public function isHTML() {
 		return !empty($this->HTMLBody);
 	}
 	
 	//! Checks if this mail is a TEXT mail
 	/*!
-		\return True if this object has a TEXT message.
-	*/
+	 * \return True if this object has a TEXT message.
+	 */
 	public function isTEXT() {
 		return !empty($this->TEXTBody);
 	}
 	
 	//! Checks if this mail is an alternative mail
 	/*!
-		\return True if this object has an alternative message.
-	*/
+	 * \return True if this object has an alternative message.
+	 */
 	public function isAlternative() {
 		return !empty($this->AltBody);
 	}
 	
 	//! Checks if this mail contains mutiple contents
 	/*!
-		\return True if this object contains multiple contents.
-	*/
+	 * \return True if this object contains multiple contents.
+	 */
 	public function isMultiContent() {
 		return ( $this->isHTML() + $this->isTEXT() + $this->containsFiles() ) > 1;
 	}
 	
 	//! Checks if the given mail address is valid
 	/*!
-		\param $email The email address.
-		\return True if this email is valid.
-	*/
+	 * \param $email The email address.
+	 * \return True if this email is valid.
+	 */
 	public static function is_email($email) {
 		return is_email($email);
 	}
 
 	//! Gets the mime type of a file.
 	/*!
-		\param $Filename The file name.
-		\return The mime type of the file.
-	*/
+	 * \param $Filename The file name.
+	 * \return The mime type of the file.
+	 */
 	public static function getMimeType($Filename) {
 		if( function_exists('finfo_open') ) {
 			$finfo = finfo_open(FILEINFO_MIME_TYPE);
@@ -488,9 +488,9 @@ BODY;
 
 	//! Escapes the string for mails.
 	/*!
-		\param $string The string to escape.
-		\return The escaped string for mails.
-	*/
+	 * \param $string The string to escape.
+	 * \return The escaped string for mails.
+	 */
 	public static function escape($string) {
 		//It seems that utf8_encode() is not sufficient, it does not work, but UTF-8 do.
 		return quoted_printable_encode(( mb_detect_encoding($string, "UTF-8") == "UTF-8" ) ? $string : utf8_encode($string));
