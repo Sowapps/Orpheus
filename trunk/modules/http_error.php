2b9cd2a7ff65359598502ae6ab67100a824e86a7
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-09-20 21:13:49 +0000

    Updating archives file. Updating http error module. Securing some folders.

--- trunk/modules/http_error.php
+++ trunk/modules/http_error.php
@@ -9,8 +9,8 @@ log_error('HTTP Error : '.$CODE, SERVLOGFILENAME, 'accessing page', null);
 
 ?>
 
-<h3>Erreur : <?php echo t('error_'.$TCODE.'_title', 'http_errors'); ?></h3>
+<h3>Error : <?php echo t('error_'.$TCODE.'_title', 'http_errors'); ?></h3>
 <hr class="tight"/>
 
 <?php echo t('error_'.$TCODE.'_text', 'http_errors'); ?><br />
-Vous pouvez nous <a href="contact.html">contacter</a> pour nous transmettre cette erreur ou retourner à  <a href="<?php echo DEFAULTLINK; ?>">la page d'accueil</a>.
+You could return to <a href="<?php echo DEFAULTLINK; ?>">the home page</a>.
