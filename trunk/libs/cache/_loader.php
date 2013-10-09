40e1a4b57338692683fcbf8141db23b9ceec0e32
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2013-10-09 12:30:12 +0000

    [FIX] Issue with Cache Interface.

--- trunk/libs/cache/_loader.php
+++ trunk/libs/cache/_loader.php
@@ -2,5 +2,6 @@
 /* Loader File for the cache sources
  */
 
+addAutoload('Cache',						'cache/cache_class.php');
 addAutoload('FSCache',						'cache/fscache_class.php');
 addAutoload('APCache',						'cache/apcache_class.php');
