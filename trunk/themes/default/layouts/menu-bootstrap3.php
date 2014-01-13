10ff4d533a39cf64eb2c95e36dbac3f8bc97c140
cartman34 <cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-01-13 21:31:37 +0000

    Styling menu.

--- trunk/themes/default/layouts/menu-bootstrap3.php
+++ trunk/themes/default/layouts/menu-bootstrap3.php
@@ -3,12 +3,10 @@
 //span allows to fill A width with a reduced height
 
 echo '
-<nav class="collapse navbar-collapse" role="navigation">
-	<ul class="nav navbar-nav">';
+<ul class="nav navbar-nav">';
 foreach( $items as $item ) {
 	echo '
 <li class="item'.(isset($item->module) ? ' '.$item->module : '').(!empty($item->current) ? ' active' : '').'"><a href="'.$item->link.'">'.$item->label.'</a></li>';
 }
 echo '
-	</ul>
-</nav>';
+</ul>';
