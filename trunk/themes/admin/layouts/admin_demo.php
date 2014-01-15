a356d87fde6fc1338cfa5632d045de6a3d37cce6
cartman34 <cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-01-15 21:56:51 +0000

    Add admin_demo module.

--- trunk/themes/admin/layouts/admin_demo.php
+++ trunk/themes/admin/layouts/admin_demo.php
@@ -7,7 +7,7 @@
             </ol>
             <div class="alert alert-success alert-dismissable">
               <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-              Welcome to SB Admin by <a class="alert-link" href="http://startbootstrap.com">Start Bootstrap</a>! Feel free to use this template for your admin needs! We are using a few different plugins to handle the dynamic tables and charts, so make sure you check out the necessary documentation links provided.
+              Welcome to Orpheus Admin Panel Demo, this SB Admin Theme is designed by <a class="alert-link" href="http://startbootstrap.com">Start Bootstrap</a> and integrated by Florent HAZARD ! Feel free to use this plugin and its templates for your admin needs! We are using a few different plugins to handle the dynamic tables and charts, so make sure you check out the necessary documentation links provided.
             </div>
           </div>
         </div><!-- /.row -->
