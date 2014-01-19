003d81d84f06d7dff8054336d9615c32220cc42c
cartman34 <cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-01-19 14:24:32 +0000

    Doing something with entity descriptors.

--- libs/sqldescriptor/entitydescriptor_class.php
+++ libs/sqldescriptor/entitydescriptor_class.php
@@ -8,14 +8,16 @@ class EntityDescriptor {
 	const DESCRIPTORCLASS='EntityDescriptor';
 	
 	public function __construct($name) {
-		$cache = new FSCache(self::DESCRIPTORCLASS, $name, filemtime(YAML::getFilePath(ENTITY_DESCRIPTOR_CONFIG_PATH.$name)));
+		$descriptorPath = ENTITY_DESCRIPTOR_CONFIG_PATH.$name;
+		$cache = new FSCache(self::DESCRIPTORCLASS, $name, filemtime(YAML::getFilePath($descriptorPath)));
 		if( !$cache->get($conf) ) {
-			$conf = YAML::build(ENTITY_DESCRIPTOR_CONFIG_PATH.$name, true);
+			$conf = YAML::build($descriptorPath, true);
 			// Build descriptor
 			
-			//	Parse Config file
+			//    Parse Config file
 			
-			//	Generate cache output
+			
+			//    Generate cache output
 			
 			// Save descriptor
 			$cache->set($conf = empty($conf) ? false : $conf->all);
@@ -23,5 +25,30 @@ class EntityDescriptor {
 		
 // 		return $conf;
 	}
+	
+	protected static $types = array();
+	public static function registerType($name, $argsParser) {
+
+
+		static::$types[$name] = array(
+			'argsParser'	=> $argsParser
+		);
+	}
 }
+
 defifn('ENTITY_DESCRIPTOR_CONFIG_PATH', 'entities/');
+
+EntityDescriptor::registerType('number', function($a1=null, $a2=null, $a3=null) {
+	$args = array('precision'=>0, 'min'=>-2147483648, 'max'=>2147483647);
+	if( !is_null($a3) ) {
+		$args['precision']	= $a1;
+		$args['min']		= $a2;
+		$args['max']		= $a3;
+	} else if( !is_null($a2) ) {
+		$args['min']		= $a1;
+		$args['max']		= $a2;
+	} else if( !is_null($a1) ) {
+		$args['max']		= $a1;
+	}
+	return $args;
+});
