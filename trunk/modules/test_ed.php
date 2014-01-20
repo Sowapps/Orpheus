bea689348157fdb51236911d8917e4d137b20133
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-01-20 14:41:56 +0000

    Implementing Entity Descriptor lib.

--- trunk/modules/test_ed.php
+++ trunk/modules/test_ed.php
@@ -8,6 +8,8 @@ foreach( $values as $value ) {
 		text('Value: '.$value);
 		$ed->validateFieldValue('a_number', $value);
 		text('OK.');
+	} catch( InvalidFieldException $e ) {
+		text($e->getMessage());
 	} catch( Exception $e ) {
 		text($e);
 	}
