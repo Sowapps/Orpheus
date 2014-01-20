7dd49bea393e2c9498efec2f69dde0350a7d53dc
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-01-20 14:36:48 +0000

    Implementing Entity Descriptor lib.

--- modules/test_ed.php
+++ modules/test_ed.php
@@ -4,5 +4,11 @@ $ed = new EntityDescriptor('users');
 
 $values = array('125', '-125');
 foreach( $values as $value ) {
-	$ed->validateFieldValue('a_number', $value);
+	try {
+		text('Value: '.$value);
+		$ed->validateFieldValue('a_number', $value);
+		text('OK.');
+	} catch( Exception $e ) {
+		text($e);
+	}
 }
