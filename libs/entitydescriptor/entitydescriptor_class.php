9b51fbb5e97278d69ccb3349d797ffbed60233de
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-01-21 13:27:21 +0000

    Implementing Entity Descriptor lib. (Type date and datetime)

--- libs/entitydescriptor/entitydescriptor_class.php
+++ libs/entitydescriptor/entitydescriptor_class.php
@@ -208,6 +208,8 @@ EntityDescriptor::registerType('date', null, function($fArgs) {
 	$value = $time;
 	
 }, function($args, &$value) {
+	text("Formatter");
+	text(var_dump($value));
 // 	$value = strtr($value, '/', '-');
 	$value = strftime('%Y-%m-%d', $value);
 });
