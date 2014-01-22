8dd35c86067dad8e91785f2d4f90396dc213c424
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-01-22 15:09:34 +0000

    Implementing Entity Descriptor lib.

---
+++ modules/test_entity.php
@@ -0,0 +1,38 @@
+<?php
+
+try {
+	if( isPOST('submitSave') ) {
+		$de = DemoEntity::load(DemoEntity::create(POST('data')));
+		text($de);
+		reportSuccess('successSave', DemoEntity::getDomain());
+	}
+} catch( UserException $e ) {
+	reportError($e);
+}
+/*
+create_date: datetime
+create_ip: ip
+edit_date: datetime
+edit_ip: ip
+name: string(6, 50)
+user_id: ref
+user_name: string(6, 50)
+published: bool=false
+*/
+
+displayReportsHTML();
+?>
+<form method="POST">
+
+<label>Name</label><input type="text" name="data[name]" /><br />
+<label>Published</label><input type="checkbox" name="data[published]" /><br />
+
+<button type="submit" name="submitSave">Save</button>
+</form>
+<style>
+<!--
+label {
+	width: 200px;
+}
+-->
+</style>
