ef038a76004bd1932090ff3ea60b775c4b0b0207
cartman34 <cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-04-23 08:55:23 +0000

    Updating top menu

--- themes/default/layouts/menu-bootstrap3.php
+++ themes/default/layouts/menu-bootstrap3.php
@@ -1,12 +1,10 @@
 <?php
-//A tag fills the li space
-//span allows to fill A width with a reduced height
-
 echo '
 <ul class="nav navbar-nav">';
 foreach( $items as $item ) {
 	echo '
-<li class="item'.(isset($item->module) ? ' '.$item->module : '').(!empty($item->current) ? ' active' : '').'"><a href="'.$item->link.'">'.$item->label.'</a></li>';
+	<li class="item'.(isset($item->module) ? ' '.$item->module : '').(!empty($item->current) ? ' active' : '').'"><a href="'.$item->link.'">'.$item->label.'</a></li>';
 }
 echo '
-</ul>';
+</ul>
+';
