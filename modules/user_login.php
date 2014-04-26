bf8dfb62016f81b44e4a793a0f056d199101e3b7
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-04-26 13:02:21 +0000

    Updating user login module

--- modules/user_login.php
+++ modules/user_login.php
@@ -1,11 +1,11 @@
 <a href="./">Basic demo</a><br />
 <br />
 <?php
-$formRegData = array();
+$formregister = array();
 if( isPOST('submitLogin') ) {
 	
 	try {
-		SiteUser::userLogin($_POST['logindata']);
+		SiteUser::userLogin($_POST['login']);
 		reportSuccess('You\'re successfully loggued in.');
 		
 	} catch(UserException $e) {
@@ -14,9 +14,9 @@ if( isPOST('submitLogin') ) {
 } else if( isPOST('submitRegister') ) {
 // 	text('Register');
 	try {
-		$formRegData = POST('regdata');
-// 		text($formRegData);
-		$Membre = SiteUser::create($formRegData);
+		$formregister = POST('register');
+// 		text($formregister);
+		$Membre = SiteUser::create($formregister);
 		reportSuccess('You\'re successfully registered.');
 	} catch(UserException $e) {
 		reportError($e);
@@ -28,53 +28,57 @@ if( User::is_login() ) {
 	echo "<p>Welcome {$USER->fullname} </p>";
 }
 ?>
-	<div class="row show-grid">
-		
-		<div class="span3">
-			<form method="POST">
-			<fieldset>
-				<legend>Log in</legend>
-				<label for="login_username">Log in with your registered account</label>
-				<input class="input" id="login_username" type="text" name="logindata[name]" placeholder="User name" required="required" /><br />
-				<input class="input" id="login_password" type="password" name="logindata[password]" placeholder="Password" required="required" /><br />
-				<button name="submitLogin" type="submit" class="btn">Login</button>
-			</fieldset>
-			</form>
-		</div>
-			
-		<div class="span6 offset1">
-			<form method="POST">
-			<fieldset>
-				<legend>Register</legend>
-				<label for="register_name">Or register your own account</label>
-				
-				<div class="row show-grid">
-					<div class="span3">
-						<label class="control-label" for="register_name">User name</label>
-						<input class="input" id="register_name" type="text" name="regdata[name]" placeholder="Enter your user name" required="required" /><br />
-						
-						<label class="control-label" for="register_fullname">Displayed name</label>
-						<input class="input" id="register_fullname" type="text" name="regdata[fullname]" placeholder="Enter your displayed name" required="required" /><br />
-						
-						<label class="control-label" for="register_email">Email</label>
-						<input class="input" id="register_email" type="text" name="regdata[email]" placeholder="Enter your email address" required="required" />
-						
-						<label class="checkbox">
-							<input name="regdata[email_public]" type="checkbox"/>Public
-						</label>
-					</div>
-				
-					<div class="span3">
-						<label class="control-label" for="register_password">Password</label>
-						<input class="input" id="register_password" type="password" name="regdata[password]" placeholder="Enter your password" required="required" /><br />
-						
-						<label class="control-label" for="register_confpassword">Confirm password</label>
-						<input class="input" id="register_confpassword" type="password" name="regdata[password_conf]" placeholder="Confirm your password" required="required" /><br />
-						
-						<button name="submitRegister" type="submit" class="btn">Register</button>
-					</div>
-				</div>
-			</fieldset>
-			</form>
-		</div>
+<div class="row show-grid">
+	
+	<div class="span3">
+		<form method="POST" role="form">
+		<fieldset>
+			<legend>Sign in</legend>
+			<div class="form-group">
+				<label for="loginUsername">Nickname</label>
+				<input type="text" class="form-control" id="loginUsername" name="login[name]" placeholder="Enter your username" />
+			</div>
+			<div class="form-group">
+				<label for="loginPassword">Password</label>
+				<input type="password" class="form-control" id="loginPassword" name="login[password]" placeholder="Enter your password" />
+			</div>
+			<button name="submitLogin" type="submit" class="btn btn-primary">Sign in</button>
+		</fieldset>
+		</form>
 	</div>
+		
+	<div class="span6 offset1">
+		<form method="POST" role="form">
+		<fieldset>
+			<legend>Register</legend>
+			
+			<div class="form-group">
+				<label for="registerUsername">Your nickname</label>
+				<input type="text" class="form-control" id="registerUsername" name="register[name]" required />
+			</div>
+			<div class="form-group">
+				<label for="registerFullname">Your public name</label>
+				<input type="text" class="form-control" id="registerFullname" name="register[fullname]" required />
+			</div>
+			<div class="form-group">
+				<label for="registerEmail">Email</label>
+				<input type="email" class="form-control" id="registerEmail" name="register[email]" required />
+			</div>
+			<div class="checkbox">
+				<label>
+					<input type="checkbox" name="register[email_public]"> I allow other members to see my email address.
+				</label>
+			</div>
+			<div class="form-group">
+				<label for="registerPassword">Your password</label>
+				<input type="password" class="form-control" id="registerPassword" name="register[password]" required />
+			</div>
+			<div class="form-group">
+				<label for="registerConfirmPassword">Confirm password</label>
+				<input type="password" class="form-control" id="registerConfirmPassword" name="register[password_conf]" required />
+			</div>
+			<button name="submitRegister" type="submit" class="btn btn-primary">Register</button>
+		</fieldset>
+		</form>
+	</div>
+</div>
