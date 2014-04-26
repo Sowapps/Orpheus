14565dfb3362625596396ef8ba03fbe68dd813d4
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-04-26 13:13:53 +0000

    Adding userslist module.

---
+++ modules/userslist.php
@@ -0,0 +1,22 @@
+<?php
+
+$users	= SiteUser::get(array(
+	'where'		=> ( User::canDo('users_seedev') ) ? '' : 'accesslevel<='.Config::get('perm_status/administrator'),
+	'orderby'	=> 'fullname ASC',
+// 	'output'	=> SQLAdapter::ARR_OBJECTS
+));
+
+displayReportsHTML();
+?>
+<h3>Users list</h3>
+<ul class="userslist">
+<?php
+foreach( $users as $user ) {
+	echo "
+	<li>
+		<span>{$user->name}</span>
+		<span>{$user->public_email}</span>
+	</li>";
+}
+?>
+</ul>
