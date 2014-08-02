5a2db86135cc7d772c35ff28beb126817c67cdb6
cartman34 <cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-08-02 21:11:39 +0000

    Improving rights

--- trunk/libs/src/siteuser_class.php
+++ trunk/libs/src/siteuser_class.php
@@ -29,6 +29,78 @@ class SiteUser extends User {
 	public function getNicename() {
 		return strtolower($this->name);
 	}
+	
+	public function getRank() {
+		$perms = array_flip(static::getAvailRoles());
+		return isset($perms[$this->accesslevel]) ? $perms[$this->accesslevel] : 'unknown_rank';
+	}
+	public static function getAppRoles() {
+		return Config::get('user_roles');
+	}
+	public function getAvailRoles() {
+		 $permStatus = static::getAppRoles();
+		 foreach( $permStatus as $status => $accesslevel ) {
+		 	if( !$this->checkPerm($accesslevel) ) {
+		 		unset($permStatus[$status]);
+		 	}
+		 }
+		 return $permStatus;
+	}
+	public function getRoleText() {
+		$status = array_flip(static::getAvailRoles());
+		return isset($status[$this->accesslevel]) ? t('role_'.$status[$this->accesslevel], static::getDomain()) : t('role_unknown', static::getDomain(), $this->accesslevel);
+	}
+	
+	public function getAdminLink($ref=0) {
+		return u('adm_user', $this->id(), empty($ref) ? '': 'ref='.getRefOfPage());
+	}
+	
+	public function getLink() {
+		return static::genLink($this->id());
+	}
+	public static function genLink($id) {
+		return u('profile', $id);
+	}
+	
+	public function canUserList($context=CRAC_CONTEXT_APPLICATION, $contextResource=null) {
+		return $this->canUserUpdate();
+	}
+	/**
+	 * @param int $context
+	 * @param SiteUser $contextResource
+	 * @return boolean
+	 */
+	public function canUserCreate($context=CRAC_CONTEXT_APPLICATION, $contextResource=null) {
+		return $this->canDo('user_edit');// Only App admins can do it.
+	}
+	
+	/**
+	 * @param int $context
+	 * @param SiteUser $contextResource
+	 * @return boolean
+	 */
+	public function canUserEdit($context=CRAC_CONTEXT_APPLICATION, $contextResource=null) {
+		if( $this->canDo('user_edit') ) { return true; }
+// 		if( $context == CRAC_CONTEXT_APPLICATION ) { return false; }
+		return false;
+	}
+	public function canSeeDevelopers($context=CRAC_CONTEXT_APPLICATION, $contextResource=null) {
+		return $this->canDo('user_seedev');// Only App admins can do it.
+	}
+	public function canUserStatus($context=CRAC_CONTEXT_APPLICATION, $contextResource=null) {
+		return $this->canDo('user_status');// Only App admins can do it.
+	}
+	public function canUserDelete($context=CRAC_CONTEXT_APPLICATION, $contextResource=null) {
+		return $this->canDo('user_delete');// Only App admins can do it.
+	}
+	public function canUserGrant($context=CRAC_CONTEXT_APPLICATION, $contextResource=null) {
+		return $this->canDo('user_grant');// Only App admins can do it.
+	}
+	
+	public function canEntityDelete($context=CRAC_CONTEXT_APPLICATION, $contextResource=null) {
+		return $this->canDo('entity_delete');// Only App admins can do it.
+	}
+	
 
 	// 		** CHECK METHODS **
 
@@ -45,13 +117,6 @@ class SiteUser extends User {
 			$data['real_password'] = $uInputData['password'];
 		}
 		return $data;
-	}
-	
-	public function getLink() {
-		return static::genLink($this->id());
-	}
-	public static function genLink($id) {
-		return u('profile', $id);
 	}
 	
 	// *** FORUM LIB ***
---
+++ trunk/modules/adm_user.php
@@ -0,0 +1,56 @@
+<?php
+	
+try {
+	$user	= SiteUser::load($Action);
+} catch(UserException $e) {
+	reportError($e);
+// 	displayReportsHTML();
+	return;
+}
+	
+if( isPOST('submitUpdate') ) {
+	try {
+		$result = $user->update(POST('user'), array('fullname', 'email', 'password', 'accesslevel'));
+		if( $result ) {
+			reportSuccess('successEdit', SiteUser::getDomain());
+		}
+	} catch(UserException $e) {
+		reportError($e, SiteUser::getDomain());
+	}
+}
+$formData = array('user' => $user->getValue());
+
+$ModuleTitle = 'Utilisateur '.$user;
+// displayReportsHTML();
+?>
+
+<form method="POST">
+
+<div class="row">
+	<div class="col-lg-6">
+		<div class="adduserform">
+		<h2>Ajouter un utilisateur</h2>
+		<div class="form-group">
+			<label>Nom</label>
+			<?php _app_htmlTextInput('user/fullname', 'form-control'); ?>
+		</div>
+		<div class="form-group">
+			<label>Email</label>
+			<?php _app_htmlTextInput('user/email', 'form-control', 'autocomplete="off"'); ?>
+		</div>
+		<div class="form-group">
+			<label>Mot de passe</label>
+			<?php _app_htmlPassword('user/password', 'class="form-control" autocomplete="off"'); ?>
+		</div>
+		<div class="form-group">
+			<label>Accréditations</label>
+			<select name="user[accesslevel]" class="form-control">
+				<?php echo htmlOptions('user/accesslevel', SiteUser::getAppRoles(), null, OPT_LABEL2VALUE, 'role_', SiteUser::getDomain()); ?>
+			</select>
+		</div>
+		<button class="btn btn-default" type="submit" name="submitUpdate">Enregistrer</button>
+		</div>
+	</div>
+</div>
+
+</form>
