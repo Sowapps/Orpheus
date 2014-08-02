a1512a06b001e065bd56b88357c156423769b431
cartman34 <cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-08-02 18:04:38 +0000

    Debug a fatal error

--- modules/home.php
+++ modules/home.php
@@ -1,14 +1,17 @@
 <?php
 if( isPOST('data') ) {
 	try {
-		
-		$tid = DemoTest::create($_POST['data']);
+		text('Create Demotest');
+		DemoTest::isFieldEditable('id');
+		text('Create Demotest');
+		$test = DemoTest::create($_POST['data']);
 		reportSuccess("Object created.");
 		
-		$test = DemoTest::load($tid);
+		$test = DemoTest::load($test);
 		reportSuccess("Object \"{$test}\" loaded, it's named \"{$test->name}\".");
 		
-		DemoTest::delete($tid);
+// 		DemoTest::delete($tid);
+		$test->remove();
 		reportSuccess("Object deleted.");
 		
 	} catch (UserException $e) {
