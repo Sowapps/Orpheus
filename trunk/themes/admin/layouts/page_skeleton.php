d01e38d18bc7604a3435ef3009aedc416d93763b
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-08-20 13:05:42 +0000

    insert settings in admin theme

--- trunk/themes/admin/layouts/page_skeleton.php
+++ trunk/themes/admin/layouts/page_skeleton.php
@@ -64,8 +64,9 @@ foreach(HTMLRendering::$cssURLs as $url) {
 	
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse navbar-ex1-collapse">
-			<?php $this->showMenu('adminmenu', 'menu-sidebar'); ?>
-		<!-- 
+			<?php
+			$this->showMenu('adminmenu', 'menu-sidebar');
+			/*
 			<ul class="nav navbar-nav side-nav">
 				<li class="active"><a href="index.html"><i class="fa fa-dashboard"></i> Dashboard</a></li>
 				<li><a href="charts.html"><i class="fa fa-bar-chart-o"></i> Charts</a></li>
@@ -85,9 +86,10 @@ foreach(HTMLRendering::$cssURLs as $url) {
 					</ul>
 				</li>
 			</ul>
-		 -->
+			*/
+			?>
 			<ul class="nav navbar-nav navbar-right navbar-user">
-			<!-- 
+			<?php /*
 				<li class="dropdown messages-dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> Messages <span class="badge">7</span> <b class="caret"></b></a>
 					<ul class="dropdown-menu">
@@ -135,19 +137,25 @@ foreach(HTMLRendering::$cssURLs as $url) {
 						<li><a href="#">View All</a></li>
 					</ul>
 				</li>
-			 -->
+			*/
+			if( AppUser::isLogged() ) {
+				?>
 				<li class="dropdown user-dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $USER; ?> <b class="caret"></b></a>
 					<ul class="dropdown-menu">
-					<!-- 
+					<?php /*
 						<li><a href="#"><i class="fa fa-user"></i> Profile</a></li>
 						<li><a href="#"><i class="fa fa-envelope"></i> Inbox <span class="badge">7</span></a></li>
 						<li><a href="#"><i class="fa fa-gear"></i> Settings</a></li>
 						<li class="divider"></li>
-					 -->
+					*/ ?>
+						<li><a href="settings.html"><i class="fa fa-gear"></i> Settings</a></li>
 						<li><a href="logout.html"><i class="fa fa-power-off"></i> Log Out</a></li>
 					</ul>
 				</li>
+			<?php
+			}
+			?>
 			</ul>
 		</div><!-- /.navbar-collapse -->
 	</nav>
