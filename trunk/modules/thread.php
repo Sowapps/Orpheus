ee3e1dcb0bce2361730e6750ef7624133b82dfe2
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-08-23 06:26:45 +0000

    Add actions button for thread emssages

--- trunk/modules/thread.php
+++ trunk/modules/thread.php
@@ -1,7 +1,9 @@
 <?php
+/* @var $USER SiteUser */
 
 $FORM_TOKEN	= new FormToken();
 
+$USER_CAN_THREADMESSAGE_MANAGE	= SiteUser::isLogged() && $USER->canThreadMessageManage();
 try {
 	isPOST() && $FORM_TOKEN->validateForm();
 	if( isPOST('submitAdd') ) {
@@ -13,6 +15,14 @@ try {
 		$input['user_name']	= $USER->fullname;
 		ThreadMessage::create($input, array('content', 'user_id', 'user_name'));
 		reportSuccess('successCreate', ThreadMessage::getDomain());
+	} else
+	if( hasPOSTKey('submitDelete', $tmID) ) {
+		if( !$USER_CAN_THREADMESSAGE_MANAGE ) {
+			SiteUser::throwException('forbiddenOperation');
+		}
+		$tm	= ThreadMessage::load($tmID);
+		$tm->remove(); unset($tm);
+		reportSuccess('successDelete', ThreadMessage::getDomain());
 	}
 } catch(UserException $e) {
 	reportError($e);
@@ -46,16 +56,27 @@ if( SiteUser::isLogged() ) {
 </div>
 <div class="row">
 	<h4>Thread</h4>
+	<form method="POST" role="form"><?php echo $FORM_TOKEN; ?>
 	<ul class="list-group">
 <?php
 foreach( ThreadMessage::get(array('orderby' => 'create_date')) as $tm ) {
 // 			<span class="badge">14</span>
 	echo '
-		<li class="list-group-item">['.$tm->getAdaptiveDate().'] <b>'.escapeText($tm->user_name).'</b> : '.$tm.'</li>';
+		<li class="list-group-item">
+			'.($USER_CAN_THREADMESSAGE_MANAGE ? '<div class="btn-group tm-actions"><button name="submitDelete['.$tm->id().']" type="submit" class="btn btn-default"><i class="fa fa-trash"></i></button></div>' : '').'
+			['.$tm->getAdaptiveDate().'] <b>'.escapeText($tm->user_name).'</b> : '.$tm.'</li>';
 }
 ?>
 	</ul>
+	</form>
 </div>
 
 	</div>
 </div>
+<style>
+.tm-actions {
+	position: absolute;
+	top:	0;
+	right:	0;
+}
+</style>
