33fac0243ee0003ccac3eaa643dec07cddabe177
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-10-10 08:53:12 +0000

    Documenting cast generating enw doc

--- doc/lib_initernalization.php
+++ doc/lib_initernalization.php
@@ -7,7 +7,7 @@
  * The initernationalization library is a basic internationalization library using ini files.\n
  * The basic language is not english by default, but an internal language, called English CamelCase.\n
  * global.ini is the language file for the overall website, update it carefully.\n
- * Add one file per domain you want to translate, ie one or more per library you develop.\n
+ * Add one file per domain you want to translate, i.e. one or more per library/class you develop.\n
  * \n
  * This package is very recommended even if you don't use the internationalization because it's light and bring all required functions.\n
  * 
