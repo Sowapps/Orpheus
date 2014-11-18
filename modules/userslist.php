322d167b61512666030efbbad70c254404f5cbaa
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2014-11-18 15:14:37 +0000

    issue with members module

--- modules/userslist.php
+++ modules/userslist.php
@@ -1,7 +1,7 @@
 <?php
 
 $users	= SiteUser::get(array(
-	'where'		=> ( SiteUser::loggedCanDo('users_seedev') ) ? '' : 'accesslevel<'.Config::get('perm_status/administrator'),
+	'where'		=> SiteUser::loggedCanDo('user_seedev') ? '' : 'accesslevel<'.Config::get('perm_status/administrator'),
 	'orderby'	=> 'fullname ASC',
 // 	'output'	=> SQLAdapter::ARR_OBJECTS
 ));
