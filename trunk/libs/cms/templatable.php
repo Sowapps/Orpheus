3592b61213d50cb30f8b6a22bb800ff8d3af11e1
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2015-04-18 12:29:02 +0000

    Updating CMS lib

---
+++ trunk/libs/cms/templatable.php
@@ -0,0 +1,11 @@
+<?php
+
+
+abstract class Templatable {
+	
+// 	public function includeTemplate($model);
+
+
+// 	public function includeInlay($identifier, $model, $max=1);
+	
+}
--- trunk/libs/cms/templateanalyzer.php
+++ trunk/libs/cms/templateanalyzer.php
@@ -1,18 +1,83 @@
 <?php
 
 
-class TemplateAnalyzer {
+class TemplateAnalyzer extends Templatable {
+	
+	private $inlay;
+	private $model;
+	private $renderer;
+	
+	private $contents;
+	
+	/**
+	 * @param string|Inlay $inlay Inlay or model
+	 */
+	public function __construct($inlay, $renderer) {
+		if( is_string($inlay) ) {
+			$this->inlay	= null;
+			$this->model	= $inlay;
+		} else {
+			$this->inlay	= $inlay;
+			$this->model	= $inlay ? $inlay->getModel() : null;
+		}
+		$this->renderer	= $renderer;
+	}
+	
+	public function analyze() {
+		$this->contents	= array();
+		$modelPath	= $this->renderer->getModelPath($this->getModel());
+		if( !is_readable($modelPath) ) {
+			throw new Exception('TemplateAnalyzer Exception, unable to find model "'.$this->getModel().'"');
+		}
+		$View	= $this;
+		$Inlay	= $this->getInlay();
+		include $modelPath;
+		return $this->contents;
+	}
 
-	public function includeTemplate($model) {
-		return '';
+	// Could contains an inlay
+	public function includeTemplate($identifier, $model) {
+		$tplAnalyzer	= new static($model, $this->getRenderer());
+		$this->contents[$identifier]	= array($tplAnalyzer->analyze());
+	}
+
+	// Could contains another inlay
+	public function includeInlay($identifier, $model, $min=1, $max=1) {
+		$max	= max($min, $max);
+		$inlays	= Inlay::getByIdentifier($identifier, $max);
+		$count	= 0;
+		$this->contents[$identifier]	= array();
+		foreach( $inlays as $inlay ) {
+// 			$tplAnalyzer->setInlay($inlay);
+			$tplAnalyzer	= new static($inlay, $this->getRenderer());
+			$this->contents[$identifier][]	= $tplAnalyzer->analyze();
+			$count++;
+		}
+		for( ; $count<$min; $count++ ) {
+			$tplAnalyzer	= new static($model, $this->getRenderer());
+			$this->contents[$identifier][]	= $tplAnalyzer->analyze();
+		}
 	}
 	
-	public function includeInlay($identifier, $model, $max=1) {
-		
+	public function setInlay(Inlay $inlay) {
+		$this->inlay	= $inlay;
+		$this->setModel($inlay->getModel());
 	}
 	
-// 	public function includeInlayList($identifier, $model, $max=0) {
-		
-// 	}
+	public function getInlay() {
+		return $this->inlay;
+	}
+	
+	public function setModel($model) {
+		$this->model	= $model;
+	}
+	
+	public function getModel() {
+		return $this->model;
+	}
+	
+	public function getRenderer() {
+		return $this->renderer;
+	}
 	
 }
