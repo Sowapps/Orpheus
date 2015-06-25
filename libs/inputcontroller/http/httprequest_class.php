2ddf07bf66a1b620bfef279567ffba5f9a3163da
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2015-06-25 20:26:57 +0000

    Controller things

--- libs/inputcontroller/http/httprequest_class.php
+++ libs/inputcontroller/http/httprequest_class.php
@@ -4,8 +4,19 @@
 class HTTPRequest extends InputRequest {
 
 	protected $method;
+	protected $contentType;
 // 	protected $query;// Parameters
-// 	protected $body;// Input
+// 	protected $input;// Input
+
+	/**
+	 * @see InputRequest::__construct()
+	 */
+	public function __construct($method, $path, $parameters, $headers, $contentType, $input) {
+		parent::__construct($path, $parameters, $input);
+		$this->method		= $method;
+		$this->contentType	= $contentType;
+	}
+
 	
 	/**
 	 * Find a matching route according to the request
@@ -38,5 +49,30 @@ class HTTPRequest extends InputRequest {
 	 */
 	public function getMethod() {
 		return $this->method;
+	}
+	
+	/**
+	 * Get the method
+	 * @return string
+	 */
+	public function generateFromEnvironment() {
+
+		$input	= null;
+// 		list($contentType, $contentOptions)	= explodeList(';', $_SERVER['CONTENT_TYPE'], 2);
+		list($contentType)	= explodeList(';', $_SERVER['CONTENT_TYPE'], 2);
+		$contentType	= trim($contentType);
+		if( $contentType === 'application/json' ) {
+// 		if( isset($_SERVER['CONTENT_TYPE']) && strpos(, 'application/json')!==false ) {
+			$input	= json_decode(file_get_contents('php://input'), true);
+			if( $input === null ) {
+				throw new Exception('malformedJSONBody', HTTP_BAD_REQUEST);
+			}
+		} else if( isset($_POST) ) {
+			$input	= $_POST;
+		}
+// 		$FORMAT	= isGET('format') ? strtolower(GET('format')) : 'json';
+// 		$PATH	= GET('_path');
+// 		$METHOD	= $_SERVER['REQUEST_METHOD'];
+		return new static($_SERVER['REQUEST_METHOD'], $path, $_GET, getallheaders(), $contentType, $input);
 	}
 }
