eed120f55994ee74efbbe95be879d8bb8c0a5fb9
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2015-07-02 14:40:40 +0000

    Controller

--- trunk/libs/inputcontroller/controller_class.php
+++ trunk/libs/inputcontroller/controller_class.php
@@ -8,6 +8,6 @@ abstract class Controller {
 	 * @param InputRequest $request
 	 * @return OutputRequest
 	 */
-	public function run(InputRequest $request);
+// 	public function run(InputRequest $request);
 	
 }
