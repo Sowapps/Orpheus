e8faf7e56c798ec94aa4ebad526be2ff96422187
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2015-07-03 07:21:57 +0000

    url route resolver

--- trunk/web/themes/default/layouts/app/home.php
+++ trunk/web/themes/default/layouts/app/home.php
@@ -61,8 +61,8 @@ label label-warning
 	<fieldset>
 		<legend>Try to create you own DemoTest object</legend>
 		<div class="form-group">
-			<label class="w200">Create it from a new value</label>
-			<input class="w300" type="text" name="data[name]" placeholder="Type new value, longer than 10 characters">
+			<label for="inputValue">Create it from a new value</label>
+			<input class="form-control" type="text" name="data[name]" placeholder="Type new value, longer than 10 characters" id="inputValue">
 		</div>
 		<span class="help-block">Submit a new value to see this working test in action.</span>
 		<button id="submitDemoTest" type="submit" class="btn btn-primary">Insert it !</button>
