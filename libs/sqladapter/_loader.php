f62a53b6b79264927de26bf93853559c3b78e3c8
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2015-07-04 11:13:15 +0000

    Updating SQL Adapter to new Orpheus

---
+++ libs/sqladapter/_loader.php
@@ -0,0 +1,12 @@
+<?php
+/** SQL Adapter Library
+ * 
+ * SQL Adapter library brings sql adapters for DBMS
+ */
+
+addAutoload('SQLAdapter',					'sqladapter/SQLAdapter');
+addAutoload('SQLAdapter_MySQL',				'sqladapter/SQLAdapter_MySQL');
+addAutoload('SQLAdapter_MSSQL',				'sqladapter/SQLAdapter_MSSQL');
+addAutoload('SQLAdapter_PgSQL',				'sqladapter/SQLAdapter_PgSQL');
+
+require_once __DIR__.'/_pdo.php';
