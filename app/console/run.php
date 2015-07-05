4816ca1bb00fd1c567ebfb922a860c42ce1774c3
Cartman34 <Cartman34@02c0ffea-ea6c-4a3b-9ea8-dcbbc747c5a4>
2015-07-05 08:28:20 +0000

    Console access

--- app/console/run.php
+++ app/console/run.php
@@ -4,4 +4,6 @@
 // defifn('ORPHEUSPATH',		dirpath(__FILE__));
 // define('ACCESSPATH',	dirname(__FILE__));
 // $REQUEST_TYPE	= 'Console';
+echo getcwd();
+
 require_once '../../system/bootstrap.php';
