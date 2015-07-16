b6563b6d4640392df8c441c56a4d6b32cf833e7d
Florent <florent@sowapps.com>
2015-07-16 14:29:43 +0200

    Add admin http controller

--- libs/src/controllers/admin/AdminHTTPController.php
+++ libs/src/controllers/admin/AdminHTTPController.php
@@ -1,6 +1,6 @@
 <?php
 
-class AdminHTTPController extends HTTPController {
+abstract class AdminHTTPController extends HTTPController {
 
 	public function preRun(HTTPRequest $request) {
 		HTMLRendering::setDefaultTheme('admin');
