629cd9984ee3b3d0f56b9b179404037ef40ac0ad
Florent <florent@sowapps.com>
2015-11-24 18:58:27 +0100

    $downloadURL

---
+++ libs/src/controllers/DownloadController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class LogoutController extends HTTPController {
+	
+	/**
+	 * @param HTTPRequest $request The input HTTP request
+	 * @return HTTPResponse The output HTTP response
+	 * @see HTTPController::run()
+	 */
+	public function run(HTTPRequest $request) {
+		
+		$downloadURL	= GlobalConfig::instance()->get('download_url');
+		
+		return new RedirectHTTPResponse($downloadURL ? $downloadURL : 'home');
+	}
+
+	
+}
