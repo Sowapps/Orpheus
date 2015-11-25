b7ce3f29f1c6caad260e3d606a51a9d03a513bf0
Florent <florent@sowapps.com>
2015-11-25 10:52:08 +0100

    Admin demo

--- app/web/themes/admin/layouts/app/admin_demo.php
+++ app/web/themes/admin/layouts/app/admin_demo.php
@@ -3,7 +3,7 @@ HTMLRendering::useLayout('page_skeleton');
 ?>
 <div class="row">
 	<div class="col-lg-12">
-		<h1>Dashboard <small>Statistics Overview</small></h1>
+<!-- 		<h1>Dashboard <small>Statistics Overview</small></h1> -->
 		<ol class="breadcrumb">
 			<li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
 		</ol>
