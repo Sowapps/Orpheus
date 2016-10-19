c6a10306f2b1e3c7c3db708afc0ced18a50ee2ef
Loenix <florent@sowapps.com>
2016-10-19 07:56:21 +0200

    Clean used libraries

--- configs/libraries.php
+++ configs/libraries.php
@@ -1,7 +1,7 @@
 <?php
-/** \file
+/**
  * Declare the libraries you want to use.
- *
+ * 
  * @page librariesLoader Libraries Loader
  * 
  */
@@ -9,20 +9,6 @@
 // Libs to call loaders, you could use not included libs but you will have to call using()
 // Order is important
 $Libraries	= array(
-	// Core
-// 	'core',
-// 	'config',
-// 	'hooks',
-// 	'cache',
-	// Config
-// 	'initernationalization',
-// 	'yaml',
-	// App specific
-// 	'sqladapter',
-// 	'inputcontroller',
-// 	'publisher',
-// 	'entitydescriptor',
-// 	'rendering',
 	// Additional
 	'twigrenderer',
 	// Your sources
