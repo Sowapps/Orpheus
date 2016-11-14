8d1f21e8091e0c62194329af52ab04644c759122
Loenix <florent@sowapps.com>
2016-11-14 16:12:27 +0100

    Move some constants to defaults file

--- configs/constants.php
+++ configs/constants.php
@@ -37,36 +37,6 @@ defifn('STATIC_URL',		SITEROOT.'static/');
 // defifn('JSURL',				SITEROOT.'js/');
 defifn('IMAGESURL',			STATIC_URL.'images/');
 
-// Routes' contants
-defifn('ROUTE_HOME',			'home');
-define('ROUTE_LOGIN',			'login');
-define('ROUTE_LOGOUT',			'logout');
-define('ROUTE_FILE_DOWNLOAD',	'file_download');
-define('ROUTE_DOWNLOAD_LATEST',		'download_latest');
-define('ROUTE_DOWNLOAD_RELEASES',	'download_releases');
-// define('ROUTE_DASHBOARD',		'user_dashboard');
-
-define('ROUTE_ADM_DEMO',		'admin_demo');
-define('ROUTE_ADM_HOME',		ROUTE_ADM_DEMO);
-define('ROUTE_ADM_USERS',		'adm_users');
-define('ROUTE_ADM_USER',		'adm_user');
-define('ROUTE_ADM_MYSETTINGS',	'adm_mysettings');
-
-define('ROUTE_DEV_HOME',		'dev_home');
-define('ROUTE_DEV_SYSTEM',		'dev_system');
-define('ROUTE_DEV_COMPOSER',	'dev_composer');
-define('ROUTE_DEV_ENTITIES',	'dev_entities');
-define('ROUTE_DEV_LOGS',		'dev_loglist');
-define('ROUTE_DEV_LOG_VIEW',	'dev_log_view');
-define('ROUTE_DEV_APPTRANSLATE',	'dev_app_translate');
-
-// Route
-defifn('DEFAULTROUTE',			ROUTE_HOME);
-defifn('DEFAULTMEMBERROUTE',	ROUTE_ADM_DEMO);
-// defifn('DEFAULTMOD',		'home');
-defifn('DEFAULTHOST',		'yourdomain.com');
-defifn('DEFAULTPATH',		'');
-
 defifn('AUTHORNAME',		'Your name');
 defifn('SITENAME',			'Your App Name');// See also translation app_name
 defifn('ADMINEMAIL',		'contact@yourdomain.com');
